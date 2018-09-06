81dad7bb9d7edac1d11be30f598380ca29b3cc47
zamhaq <haque864@gmail.com>
2018-09-07 00:43:29 +0530

    Fix context menus not triggering onClick due to id's

--- src/components/File.js
+++ src/components/File.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 import fileicon from "./file.png";
 
-const File = ({ file }) => (
-  <div>
-    <img style={{ display: "block" }} src={fileicon} alt="" />
-    {file.name}
-    {`.`}
-    {file.ext}
-  </div>
-);
+import "./reactContextMenu.css";
+
+class File extends Component {
+  handleClick = (e, data) => {
+    console.log("deleting");
+    console.log(data.name);
+  };
+  render() {
+    const { file } = this.props;
+    return (
+      <div>
+        <ContextMenuTrigger id={`fileMenu-${file.name}`}>
+          <div>
+            <img style={{ display: "block" }} src={fileicon} alt="" />
+            {file.name}
+            {`.`}
+            {file.ext}
+          </div>
+        </ContextMenuTrigger>
+        <ContextMenu id={`fileMenu-${file.name}`}>
+          <MenuItem data={{ name: file.name }} onClick={this.handleClick}>
+            Delete
+          </MenuItem>
+        </ContextMenu>
+      </div>
+    );
+  }
+}
 
 export default File;
--- src/components/Folder.js
+++ src/components/Folder.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 import { withRouter } from "react-router-dom";
 import foldericon from "./folder.png";
 
-const Folder = ({ currentpath, folder, history }) => {
-  return currentpath === "/" ? (
-    <div
-      onDoubleClick={() => {
-        history.push(`${currentpath}${folder.name}`);
-      }}
-    >
-      <img style={{ display: "block" }} src={foldericon} alt="" />
-      <h5>{folder.name}</h5>
-    </div>
-  ) : (
-    <div
-      onDoubleClick={() => {
-        history.push(`${currentpath}/${folder.name}`);
-      }}
-    >
-      <img style={{ display: "block" }} src={foldericon} alt="" />
-      <h5>{folder.name}</h5>
-    </div>
-  );
-};
+import "./reactContextMenu.css";
+
+class Folder extends Component {
+  handleClick = (e, data) => {
+    console.log("deleting");
+    console.log(data.name);
+  };
+  render() {
+    const { currentpath, folder, history } = this.props;
+    return (
+      <div
+        onDoubleClick={() => {
+          currentpath === "/"
+            ? history.push(`${currentpath}${folder.name}`)
+            : history.push(`${currentpath}/${folder.name}`);
+        }}
+      >
+        <ContextMenuTrigger id={`folderMenu-${folder.name}`}>
+          <div>
+            <img style={{ display: "block" }} src={foldericon} alt="" />
+            <h5>{folder.name}</h5>
+          </div>
+        </ContextMenuTrigger>
+        <ContextMenu id={`folderMenu-${folder.name}`}>
+          <MenuItem data={{ name: folder.name }} onClick={this.handleClick}>
+            Delete
+          </MenuItem>
+        </ContextMenu>
+      </div>
+    );
+  }
+}
 
 export default withRouter(Folder);
